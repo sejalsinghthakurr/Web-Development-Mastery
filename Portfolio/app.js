6e73c09c1b80ab922c0ffea91b293ba66fe7e32d
sejalsinghthakurr <sejalsinghthakur.01@gmail.com>
2026-04-16 14:40:01 +0530

    Remove Portfolio Folder from tracking

--- Portfolio/app.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello JavaScript");
-let penPrice = 47;
-let pencilPrice = 42;
-// let output = "The total price is : " + (penPrice + penPrice) + "Rupees.";
-let output = `The total price is:  ${penPrice + pencilPrice} + Rupees.` //This is called Template LiteraLs
-console.log(output);
